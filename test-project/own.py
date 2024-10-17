1e56e636d69fde6c36e940e874b3ceef0a3b876a
kaylie1031 <70783110+kaylie1031@users.noreply.github.com>
2024-10-17 23:15:17 +0800

    17-10-24_2

--- test-project/own.py
+++ test-project/own.py
@@ -7,6 +7,16 @@ rb.pos = 100,56
 WIDTH = 200
 HEIGHT = rb.height +20
 
+rb.topright = 0,10
+
 def draw():
     screen.clear()
     rb.draw()
+
+def update():
+    rb.left += 2
+    if rb.left> WIDTH:
+        rb.right = 0
+
+def on_mouse_down():
+    rb.y -=1
